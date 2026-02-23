5107fee0fb5a3ce7234be7d48981ae8c692c3f8c
adamdon <mail@adamdon.co.uk>
2026-02-23 13:27:28 +0000

    Restructured files

--- node-js-express/src/api/deleteOne.js
+++
@@ -1,6 +0,0 @@
-import { Item } from "../utilities/database.js";
-
-export const deleteOne = async (req, res) => {
-    const deletedItems = await Item.deleteMany({ ref: req.body.ref });
-    res.send(deletedItems);
-};
--- node-js-express/src/api/readAll.js
+++
@@ -1,6 +0,0 @@
-import { Item } from "../utilities/database.js";
-
-export const readAll = async (req, res) => {
-    const items = await Item.find();
-    res.send(items);
-};
